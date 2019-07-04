ade54416330b0df33ff89b40dc109259e8153f66
Martin Fabbri <martin.fabbri.a@gmail.com>
2019-07-04 17:27:12 +0000

    Third Factor. Backing services.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
-import { MongoClient } from 'mongodb'
+import path from 'path'
+import express from 'express'
+import proxy from 'express-http-proxy'
 import dotenv from 'dotenv'
 
+const SERVER_PORT = 8080
+
 dotenv.config()
-const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/foo'
 
-MongoClient.connect(mongoUri, (err, db) => {
-    if (err) {
-        console.log('cannot connect to MongoDB', db)
-    } else {
-        console.log('Connected to MongoDB!')
-    }
-})
+const baseImgUrl = process.env.BASE_IMAGE_URL
+const proxyBaseImageUrl = baseImgUrl
+    ? proxy(baseImgUrl, {
+          proxyReqPathResolver: req => {
+              const newPath = baseImgUrl + req.path
+              console.log(newPath)
+              return newPath
+          },
+      })
+    : express.static(path.join(__dirname, 'public/images'))
 
-console.log('Server: http://localhost:4000')
+const app = express()
+
+app.use('/images', proxyBaseImageUrl)
+app.listen(SERVER_PORT)
+
+console.log(`Server: http://localhost:${SERVER_PORT}`)
